a05713395d12f9d904864ba8e03f8b371a01b0d8
Yash Sharma <yash9058990@gmail.com>
2024-07-07 18:38:43 +0530

    Pdf file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/build/', // Adjust as per your deployment environment
-  build: {
-    outDir: 'build', // Output directory for production build
-  },
+  base: './',  // Ensure base is configured to generate paths without leading slash
+  // other configuration options
 });
